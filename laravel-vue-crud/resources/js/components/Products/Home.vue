0d0947fbeef273b19f3a21be7143677dbe461744
zuberek <maras2132@gmail.com>
2022-11-29 21:50:59 +0100

    home.vue cleaned

--- laravel-vue-crud/resources/js/components/Products/Home.vue
+++ laravel-vue-crud/resources/js/components/Products/Home.vue
@@ -1,119 +1,12 @@
 <template>
   <TabMenu :model="items"> </TabMenu>
-  
 </template>
 <script>
 import { ref } from 'vue';
-import { useToast } from 'primevue/usetoast';
 import { defineComponent } from 'vue';
 export default defineComponent({
   setup() {
-    const toast = useToast();
-
-    const showSuccess = () => {
-      toast.add({
-        severity: 'success',
-        summary: 'Success Message',
-        detail: 'Message Content',
-        life: 3000,
-      });
-    };
-    const showInfo = () => {
-      toast.add({
-        severity: 'info',
-        summary: 'Info Message',
-        detail: 'Message Content',
-        life: 3000,
-      });
-    };
-    const showWarn = () => {
-      toast.add({
-        severity: 'warn',
-        summary: 'Warn Message',
-        detail: 'Message Content',
-        life: 3000,
-      });
-    };
-    const showError = () => {
-      toast.add({
-        severity: 'error',
-        summary: 'Error Message',
-        detail: 'Message Content',
-        life: 3000,
-      });
-    };
-    const showTopLeft = () => {
-      toast.add({
-        severity: 'info',
-        summary: 'Info Message',
-        detail: 'Message Content',
-        group: 'tl',
-        life: 3000,
-      });
-    };
-    const showBottomLeft = () => {
-      toast.add({
-        severity: 'warn',
-        summary: 'Warn Message',
-        detail: 'Message Content',
-        group: 'bl',
-        life: 3000,
-      });
-    };
-    const showBottomRight = () => {
-      toast.add({
-        severity: 'success',
-        summary: 'Success Message',
-        detail: 'Message Content',
-        group: 'br',
-        life: 3000,
-      });
-    };
-    const showSticky = () => {
-      toast.add({
-        severity: 'info',
-        summary: 'Sticky Message',
-        detail: 'Message Content',
-      });
-    };
-    const showMultiple = () => {
-      toast.add({
-        severity: 'info',
-        summary: 'Message 1',
-        detail: 'Message 1 Content',
-        life: 3000,
-      });
-      toast.add({
-        severity: 'info',
-        summary: 'Message 2',
-        detail: 'Message 2 Content',
-        life: 3000,
-      });
-      toast.add({
-        severity: 'info',
-        summary: 'Message 3',
-        detail: 'Message 3 Content',
-        life: 3000,
-      });
-    };
-    const showTemplate = () => {
-      toast.add({
-        severity: 'warn',
-        summary: 'Are you sure?',
-        detail: 'Proceed to confirm',
-        group: 'bc',
-      });
-    };
-    const onConfirm = () => {
-      toast.removeGroup('bc');
-    };
-    const onReject = () => {
-      toast.removeGroup('bc');
-    };
-    const clear = () => {
-      toast.removeAllGroups();
-    };
-
+   
     const items = ref([
       {
         label: 'Home',
@@ -143,19 +36,6 @@ export default defineComponent({
     ]);
     return {
       items,
-      showSuccess,
-      showInfo,
-      showWarn,
-      showError,
-      showTopLeft,
-      showBottomLeft,
-      showBottomRight,
-      showSticky,
-      showMultiple,
-      showTemplate,
-      onConfirm,
-      onReject,
-      clear,
     };
   },
 });
